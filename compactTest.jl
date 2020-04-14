d4d64129b08ad6cedf316c5c526fcaf79f0fb951
tuukkamattlar <tuukka.mattlar@aalto.fi>
2020-04-14 23:35:39 +0300

    initial

---
+++ compactTest.jl
@@ -0,0 +1,229 @@
+using JuMP # used for mathematical programming
+using Plots
+using Gurobi
+using DataFrames
+using VegaLite
+using VegaDatasets
+using CSV
+
+#NOTE: Technologies used
+# 1 nuclear
+# 2 coal slow
+# 3 coal fast
+# 4 gas
+# 5 wind
+# 6 solar
+# 7 water
+
+
+#GENERATE AND IMPORT DATA
+include("compactvalues.jl")
+
+#APPLY CARBON TAX
+TAX = 1000
+
+#LAUNCH MODEL
+model = Model(Gurobi.Optimizer)
+
+#VARIABLES
+@variable(model, 0 <= Charge[Storage, Hours])
+@variable(model, 0 <= Discharge[Storage, Hours])
+@variable(model, 0 <= StorageLevel[Storage, Hours])
+@variable(model, 0 <= AdditionalStorage[Storage])
+#@variable(model, 0 <= SatisfiedDemand[Hours])
+@variable(model, 0 <= EnergyProduction[Hours, Tech])
+@variable(model, 0 <= AdditionalCapacity[Tech])
+
+
+#CONSTRAINTS
+# PRODUCTION PRO TECHNOLOGY
+@constraint(model, [h in Hours, t in Tech], 0 <= EnergyProduction[h,t])
+@constraint(model, [h in Hours, t in Tech], EnergyProduction[h,t]
+    <= capFactor[h,t]*(iniCapT[t] + AdditionalCapacity[t])
+)
+@constraint(model, [t in Tech], 0 <= AdditionalCapacity[t])
+@constraint(model, [t in Tech], AdditionalCapacity[t] + iniCapT[t] <= maxCapT[t])
+
+# STORAGE
+@constraint(model, [s in Storage, h in Hours], 0 <= StorageLevel[s,h])
+@constraint(model, [s in Storage, h in Hours], StorageLevel[s,h] <= maxCapS[s])
+@constraint(model, [s in Storage, h in Hours], 0 <= Charge[s,h])
+@constraint(model, [s in Storage, h in Hours], Charge[s,h] <= chargeMax[s])
+@constraint(model, [s in Storage, h in Hours], 0 <= Discharge[s,h])
+@constraint(model, [s in Storage, h in Hours], Discharge[s,h] <= dischargeMax[s])
+
+# MEETING THE DEMAND
+@constraint(model, [h in Hours],
+    sum(EnergyProduction[h,t] for t in Tech)
+    + sum(Discharge[s,h] - Charge[s,h] for s in Storage)
+    == Demand[h]
+)
+
+# RAMPING LIMITATIONS
+@constraint(model, [h in Hours[Hours.>1], t in Tech],
+    EnergyProduction[h,t] - EnergyProduction[h-1,t]
+    >= -rampDownMax[t]*(iniCapT[t] + AdditionalCapacity[t])
+)
+@constraint(model, [h in Hours[Hours.>1], t in Tech],
+    EnergyProduction[h,t] - EnergyProduction[h-1,t]
+    <= rampUpMax[t]*(iniCapT[t] + AdditionalCapacity[t])
+)
+
+
+#EXPRESSIONS
+
+useC = @expression(model, sum(variableCostT[t]*EnergyProduction[h,t]
+    + fixedCostT[t]*(iniCapT[t] + AdditionalCapacity[t]) for t in Tech, h in Hours)
+)
+einvC = @expression(model, sum(AdditionalCapacity[t]*invCostT[t]*(1/expLifeTimeT[t]) for t in Tech))
+sinvC = @expression(model, sum(AdditionalStorage[s]*invCostS[s]*(1/expLifeTimeS[s]) for s in Storage))
+taxC = @expression(model, sum(EnergyProduction[h,t]*TAX*(1/proEmisFactor[t-1]) for t in CarbonTech, h in Hours))
+rampExp = @expression(model, sum(((EnergyProduction[h,1] - EnergyProduction[h-1,1])^2)*1000 for h in Hours[Hours.>1]))
+
+
+#OBJECTIVE
+@objective(model, Min , useC + einvC + sinvC + taxC + rampExp)
+
+
+
+#OPTIMIZE
+optimized = optimize!(model)
+termination_status(model)
+
+
+
+#RESULTS
+
+Results = Matrix{Float64}(undef, length(Hours), 15)
+for h in Hours
+    Results[h, 1] = value(EnergyProduction[h,1])
+    Results[h, 2] = value(EnergyProduction[h,2])
+    Results[h, 3] = value(EnergyProduction[h,3])
+    Results[h, 4] = value(EnergyProduction[h,4])
+    Results[h, 5] = value(EnergyProduction[h,5])
+    Results[h, 6] = value(EnergyProduction[h,6])
+    Results[h, 7] = value(EnergyProduction[h,7])
+    #Results[h, 8] = value(EnergyProduction[h,8])
+    Results[h, 9] = value(Charge[1, h])
+    Results[h, 10] = value(Charge[2, h])
+    Results[h, 11] = value(Charge[3, h])
+    Results[h, 12] = sum(Results[h, i] for i in 1:8)
+    Results[h, 13] = sum(Results[h, i] for i in 9:11)
+    #Results[h, 9] = Results[h,8]-Demand[h]
+end
+
+plot(Results[:,1:7], label = ["Nuclear" "CHP" "CHP2" "Gas" "Wind" "PV" "Hydro"])
+
+plot(Results[:,13])
+
+pt = Matrix{Float64}(undef, length(Hours), 15)
+for h in Hours
+    pt[h, 1] = Results[h,1]
+    pt[h, 2] = pt[h,1] + Results[h,2]
+    pt[h, 3] = pt[h,2] + Results[h,3]
+    pt[h, 4] = pt[h,3]+ Results[h,4]
+    pt[h, 5] = pt[h,4]+ Results[h,5]
+    pt[h, 6] = pt[h,5]+Results[h,6]
+    pt[h, 7] = pt[h,6]+Results[h,7]
+end
+plot(pt[:,1:7], label = ["Nuclear" "CHP" "CHP2" "Gas" "Wind" "PV" "Hydro"])
+
+
+
+#SAVE THE OBJECTIVE
+
+
+
+
+
+#LOOP FOR TESTING
+L = 100
+result = Matrix{Float64}(undef, L, 15)
+RENW = Matrix{Float64}(undef, length(Hours), 2)
+for i in 1:L
+    TAX = i*50
+    taxC = @expression(model, sum(EnergyProduction[h,t]*TAX*(1/proEmisFactor[t-1]) for t in CarbonTech, h in Hours))
+    @objective(model, Min , useC + einvC + sinvC + taxC + rampExp)
+    optimized = optimize!(model)
+    result[i,1] = objective_value(model)
+    for h in Hours
+        RENW[h,1] = value(EnergyProduction[h,5]) + value(EnergyProduction[h,6]) + value(EnergyProduction[h,7])
+        RENW[h,2] = value(EnergyProduction[h,2]) + value(EnergyProduction[h,3])+value(EnergyProduction[h,4])
+    end
+    result[i,2] = sum(RENW[h,1] for h in Hours)/sum(RENW[h,2]+RENW[h,1] for h in Hours )
+end
+
+plot(result[:,1])
+plot(result[:,2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rf = convert(DataFrame, Results)
+using RCall
+@rlibrary ggplot2
+
+ggplot(data=Results, aes(x=rf, group=cut, fill=cut)) +
+    geom_density(adjust=1.5, position="fill") +
+    theme_ipsum()
+)
+
+
+CSV.write("exampletoplot.csv",rf;append=true)
+
+
+p = rf %>%
+  # Compute the proportions:
+group_by(year) %>%
+mutate(freq = n / sum(n)) %>%
+ungroup() %>%
+
+# Plot
+ggplot( aes(x=year, y=freq, fill=name, color=name, text=name)) +
+  geom_area(  ) +
+  scale_fill_viridis(discrete = TRUE) +
+  scale_color_viridis(discrete = TRUE) +
+  theme(legend.position="none") +
+  ggtitle("Popularity of American names in the previous 30 years") +
+  theme_ipsum() +
+  theme(legend.position="none")
+ggplotly(p, tooltip="text")
+
+
+
+
+
+
+
+
+dataset("Results") |>
+@vlplot(
+    width=400,
+    height=100,
+    :area,
+    transform=[
+        {density="x1",bandwidth=0.3,groupby=["Major_Genre"],extent=[0, 10],counts=true,steps=50}
+    ],
+    x={"value:q", title="IMDB Rating"},
+    y= {"density:q",stack=true},
+    color={"Major_Genre:n",scale={scheme=:category20}}
+)
---
+++ compactvalues.jl
@@ -0,0 +1,200 @@
+
+#Technologies used
+# 1 nuclear
+# 2 coal slow
+# 3 coal fast
+# 4 gas
+# 5 wind
+# 6 solar
+# 7 hydro
+
+#LENGTH OF INVESTIGATION
+LEN = 700
+
+################################################################################
+# SETS
+
+Tech = collect(1:7)
+CarbonTech = collect(2:4)
+RenewableTech = collect(5:7)
+Hours = collect(1:LEN)
+Storage = collect(1:3)
+
+
+
+################################################################################
+# IMPORT DATA
+
+usage = CSV.read("usedata.csv") #RENEWABLES NINJA
+dema = CSV.read("demandinfinland.csv") #FINGRID (MWh)
+locfromend = 1000
+
+sp = length(usage[:,1])-length(Hours)-locfromend
+ep  = length(usage[:,1])-locfromend
+udata = usage[sp:ep,2:7]
+
+sp = length(dema[:,1])-length(Hours)-locfromend-1
+ep  = length(dema[:,1])-locfromend-1
+dem = dema[sp:ep,5]
+
+#plot(udata[:,1])
+for h in Hours
+    udata[h, 5] = min(udata[h,1]+udata[h,2]+udata[h,3]+udata[h,4]+udata[h,5], 1)
+    udata[h, 4] = parse(Float64, dem[h])
+end
+#plot(udata[:,6])
+#plot(udata[:,5])
+#plot(udata[:,4])
+
+################################################################################
+# PARAMETERS
+
+#TECH
+iniCapT = Array{Float64}(undef, length(Tech))
+maxCapT = Array{Float64}(undef, length(Tech))
+capFactor = Matrix{Float64}(undef, length(Hours), length(Tech))
+rampUpMax = Array{Float64}(undef, length(Tech))
+rampDownMax = Array{Float64}(undef, length(Tech))
+variableCostT = Array{Float64}(undef, length(Tech))
+fixedCostT = Array{Float64}(undef, length(Tech))
+invCostT = Array{Float64}(undef, length(Tech))
+expLifeTimeT = Array{Float64}(undef, length(Tech))
+eff = Array{Float64}(undef, length(Tech))
+
+#for t in Tech
+#    iniCapT[t] = 400
+#    maxCapT[t] = 600
+#    rampUpMax[t] = 0.05
+#    rampDownMax[t] = 20
+#    variableCostT[t] = 400
+#    fixedCostT[t] = 200
+#    invCostT[t] = 10000
+#    expLifeTimeT[t] = 50
+#end
+#http://smartenergytransition.fi/en/
+#finnish-energy-system-can-be-made-100-fossil-fuel-free/
+
+# 1 nuclear
+iniCapT[1] = 2794
+maxCapT[1] = 50000
+rampUpMax[1] = 1
+rampDownMax[1] = 1
+variableCostT[1] = 37
+fixedCostT[1] = 200
+invCostT[1] = 5000000
+expLifeTimeT[1] = 60
+eff[1] = 0.4
+
+# 2 coal slow
+iniCapT[2] = 3200
+maxCapT[2] = 40000
+rampUpMax[2] = 0.1
+rampDownMax[2] = 0.6
+variableCostT[2] = 11
+fixedCostT[2] = 100
+invCostT[2] = 1600000
+expLifeTimeT[2] = 50
+eff[2] = 0.45
+
+# 3 coal fast
+iniCapT[3] = 0
+maxCapT[3] = 0
+rampUpMax[3] = 0.1
+rampDownMax[3] = 0.1
+variableCostT[3] = 11
+fixedCostT[3] = 100
+invCostT[3] = 1600000
+expLifeTimeT[3] = 50
+eff[3] = 0.4
+
+# 4 gas
+iniCapT[4] = 1000
+maxCapT[4] = 50000
+rampUpMax[4] = 0.1
+rampDownMax[4] = 0.1
+variableCostT[4] = 22
+fixedCostT[4] = 100
+invCostT[4] = 500000
+expLifeTimeT[4] = 50
+eff[4] = 0.6
+
+# 5 wind
+iniCapT[5] = 2000
+maxCapT[5] = 20000
+rampUpMax[5] = 1
+rampDownMax[5] = 1
+variableCostT[5] = 0
+fixedCostT[5] = 100
+invCostT[5] = 1200000
+expLifeTimeT[5] = 50
+eff[5] = 1
+
+# 6 solar
+iniCapT[6] = 80
+maxCapT[6] = 15000
+rampUpMax[6] = 1
+rampDownMax[6] = 1
+variableCostT[6] = 0
+fixedCostT[6] = 100
+invCostT[6] = 800000
+expLifeTimeT[6] = 50
+eff[6] = 1
+
+# 7 hydro
+iniCapT[7] = 3100
+maxCapT[7] = 50000
+rampUpMax[7] = 0.5
+rampDownMax[7] = 0.5
+variableCostT[7] = 0
+fixedCostT[7] = 100
+invCostT[7] = 1000000
+expLifeTimeT[7] = 50
+eff[7] = 1
+
+
+
+for h in Hours
+    capFactor[h, 1] = 1
+    capFactor[h, 2] = 1
+    capFactor[h, 3] = 1
+    capFactor[h, 4] = 1
+    capFactor[h, 5] = udata[h,5]
+    capFactor[h, 6] = udata[h,6]
+    capFactor[h, 7] = ((sin((h+3)/5))^2)./2 .+0.3 .+rand(Float64,1)[1]*0.2
+end
+
+#OTHER
+proEmisFactor =  Array{Float64}(undef, length(CarbonTech))
+
+for c in CarbonTech
+    proEmisFactor[c-1] = 300
+end
+
+
+
+#STORAGE
+chargeMax = Array{Float64}(undef, length(Storage))
+dischargeMax = Array{Float64}(undef, length(Storage))
+iniCapS = Array{Float64}(undef, length(Storage))
+maxCapS = Array{Float64}(undef, length(Storage))
+invCostS = Array{Float64}(undef, length(Storage))
+expLifeTimeS = Array{Float64}(undef, length(Storage))
+
+for s in Storage
+    chargeMax[s] = 40
+    dischargeMax[s] = 40
+    iniCapS[s] = 500
+    maxCapS[s] = 800
+    invCostS[s] = 400
+    expLifeTimeS[s] = 20
+end
+
+# DEMAND
+Demand = Array{Float64}(undef, length(Hours))
+
+for h in Hours
+    Demand[h] = udata[h,4]
+end
+
+#plot(capFactor)
+#plot(Demand)
