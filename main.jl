1ed3a0a7173bd4748634c8f842eade3299cb2564
tuukkamattlar <tuukka.mattlar@aalto.fi>
2020-05-13 23:46:44 +0300

    Add Nodes, Hydro

--- main.jl
+++ main.jl
@@ -1,10 +1,7 @@
-using JuMP # used for mathematical programming
-using Plots
-using Gurobi
-using DataFrames
-using VegaLite
-using VegaDatasets
-using CSV
+using JuMP, Plots, Gurobi, CSV
+#using DataFrames
+#using VegaLite
+#using VegaDatasets
 
 #NOTE: Technologies used
 # 1 nuclear
@@ -15,9 +12,7 @@ using CSV
 # 6 solar
 # 7 water
 
-#NOTE: SELECT LENGTH OF PERIOD IN VALUES.JL
-
-#GENERATE AND IMPORT DATA
+#IMPORT DATA
 include("values.jl")
 
 #APPLY CARBON TAX
@@ -27,63 +22,96 @@ TAX = 25
 model = Model(Gurobi.Optimizer)
 
 #VARIABLES
-@variable(model, 0 <= Charge[Storage, Hours])
-@variable(model, 0 <= Discharge[Storage, Hours])
-@variable(model, 0 <= StorageLevel[Storage, Hours])
-@variable(model, 0 <= AdditionalStorage[Storage])
-#@variable(model, 0 <= SatisfiedDemand[Hours])
-@variable(model, 0 <= EnergyProduction[Hours, Tech])
-@variable(model, 0 <= AdditionalCapacity[Tech])
-
+@variable(model, 0 <= Charge[Storage, Hours, Region])
+@variable(model, 0 <= Discharge[Storage, Hours, Region])
+@variable(model, 0 <= StorageLevel[Storage, Hours, Region])
+@variable(model, 0 <= AdditionalStorage[Storage, Region])
+@variable(model, 0 <= EnergyProduction[Hours, Tech, Region])
+@variable(model, 0 <= AdditionalCapacity[Tech, Region])
+@variable(model, 0 <= Trans[Hours, Region, Region])
+@variable(model, 0 <= AdditionalTrans[Region, Region])
+@variable(model, 0 <= HydroReservoirLevel[Hours, Region])
+@variable(model, 0 <= HydroOutflow[Hours, Region])
+@variable(model, 0 <= HydroOutBypass[Hours, Region])
+@variable(model, 0 <= HydroOutPower[Hours, Region])
+@variable(model, TransBin[Hours,Region, Region], Bin)
 
 #CONSTRAINTS
 # PRODUCTION PRO TECHNOLOGY
-@constraint(model, [h in Hours, t in Tech], 0 <= EnergyProduction[h,t])
-@constraint(model, [h in Hours, t in Tech], EnergyProduction[h,t]
-    <= capFactor[h,t]*(iniCapT[t] + AdditionalCapacity[t])
+@constraint(model, [h in Hours, t in Tech, r in Region], 0 <= EnergyProduction[h,t,r])
+
+#FOLLOWING ONLY FOR NON-HYDRO TECHNOLOGIES
+@constraint(model, [h in Hours, t in TechNH, r in Region],
+    EnergyProduction[h,t,r]
+    <=
+    capFactor[h,t,r]*(iniCapT[t,r]
+    + AdditionalCapacity[t,r])
 )
-@constraint(model, [t in Tech], 0 <= AdditionalCapacity[t])
-@constraint(model, [t in Tech], AdditionalCapacity[t] + iniCapT[t] <= maxCapT[t])
+#@constraint(model, [t in Tech, r in Region], 0 <= AdditionalCapacity[t,r])
+@constraint(model, [t in TechNH, r in Region],
+    AdditionalCapacity[t,r]
+    + iniCapT[t,r]
+    <=
+    maxCapT[t,r]
+)
 
 # STORAGE
-@constraint(model, [s in Storage, h in Hours], 0 <= StorageLevel[s,h])
-@constraint(model, [s in Storage, h in Hours], StorageLevel[s,h] <= maxCapS[s])
-@constraint(model, [s in Storage, h in Hours], 0 <= Charge[s,h])
-@constraint(model, [s in Storage, h in Hours], Charge[s,h] <= chargeMax[s])
-@constraint(model, [s in Storage, h in Hours], 0 <= Discharge[s,h])
-@constraint(model, [s in Storage, h in Hours], Discharge[s,h] <= dischargeMax[s])
+@constraint(model, [s in Storage, h in Hours, r in Region], 0 <= StorageLevel[s,h,r])
+@constraint(model, [s in Storage, h in Hours, r in Region], StorageLevel[s,h,r] <= maxCapS[s,r])
+@constraint(model, [s in Storage, h in Hours, r in Region], 0 <= Charge[s,h,r])
+@constraint(model, [s in Storage, h in Hours, r in Region], Charge[s,h,r] <= chargeMax[s])
+@constraint(model, [s in Storage, h in Hours, r in Region], 0 <= Discharge[s,h,r])
+@constraint(model, [s in Storage, h in Hours, r in Region], Discharge[s,h,r] <= dischargeMax[s])
+@constraint(model, [r in Region, h in Hours], Trans[h,r,r] == 0) #cannot trans ele within
+@constraint(model, [r in Region], AdditionalTrans[r,r] == 0) #cannot trans ele within
+@constraint(model, [r in Region, rr in Region, h in Hours], Trans[h, r, rr] <= transCap[r, rr] + AdditionalTrans[r, rr])
+@constraint(model, [r in Region, rr in Region, h in Hours], transCap[r,rr] + AdditionalTrans[r,rr] <= maxTransCap[r, rr])
+#@constraint(model, [r in Region, rr in Region, h in Hours], TransBin[h,rr,r]*Trans[h,rr,r] + TransBin[h,r,rr]*Trans[h,r,rr] <= Trans[h,r,rr] + Trans[h,rr,r] -1   )
+
 
 # MEETING THE DEMAND
-@constraint(model, [h in Hours],
-    sum(EnergyProduction[h,t] for t in Tech)
-    + sum(Discharge[s,h] - Charge[s,h] for s in Storage)
-    == Demand[h]
+@constraint(model, [h in Hours, r in Region],
+    sum(EnergyProduction[h,t,r] for t in Tech)
+    + sum(Discharge[s,h,r] - Charge[s,h,r] for s in Storage) + sum(Trans[h,r,from] for from in Region)
+    - sum(Trans[h,to,r] for to in Region)
+    == Demand[h, r]
 )
 
 # RAMPING LIMITATIONS
-@constraint(model, [h in Hours[Hours.>1], t in Tech],
-    EnergyProduction[h,t] - EnergyProduction[h-1,t]
-    >= -rampDownMax[t]*(iniCapT[t] + AdditionalCapacity[t])
+@constraint(model, [h in Hours[Hours.>1], t in Tech, r in Region],
+    EnergyProduction[h,t,r] - EnergyProduction[h-1,t,r]
+    >= -rampDownMax[t]*(iniCapT[t,r] + AdditionalCapacity[t,r])
 )
-@constraint(model, [h in Hours[Hours.>1], t in Tech],
-    EnergyProduction[h,t] - EnergyProduction[h-1,t]
-    <= rampUpMax[t]*(iniCapT[t] + AdditionalCapacity[t])
+@constraint(model, [h in Hours[Hours.>1], t in Tech, r in Region],
+    EnergyProduction[h,t,r] - EnergyProduction[h-1,t,r]
+    <= rampUpMax[t]*(iniCapT[t,r] + AdditionalCapacity[t,r])
 )
 
+#HYDRO
+@constraint(model, [h in Hours, r in Region], hydroMinReservoir[r] <= HydroReservoirLevel[h,r])
+@constraint(model, [h in Hours, r in Region], hydroMaxReservoir[r] >= HydroReservoirLevel[h,r])
+@constraint(model, [h in Hours[Hours.>(LEN-1)], r in Region], HydroReservoirLevel[h+1,r] == HydroReservoirLevel[h,r] + hydroInflow[h,r] - HydroOutflow[h,r])
+@constraint(model, [h in Hours, r in Region], HydroOutBypass[h,r] + HydroOutPower[h,r] == HydroOutflow[h,r])
+@constraint(model, [h in Hours, r in Region], HydroOutflow[h,r] >= hydroMinEnvFlow[r])
+@constraint(model, [h in Hours, r in Region], HydroOutPower[h,r] <= hydroReservoirCapacity[r] + AdditionalCapacity[7,r])
+@constraint(model, [h in Hours, r in Region], HydroOutPower[h,r] == EnergyProduction[h,7,r])
+@constraint(model, [r in Region], hydroReservoirCapacity[r] + AdditionalCapacity[7,r] <= hydroMaxOverall[r])
 
 #EXPRESSIONS
 
-useC = @expression(model, sum(variableCostT[t]*EnergyProduction[h,t]/eff[t]
-    + fixedCostT[t]*(iniCapT[t] + AdditionalCapacity[t]) for t in Tech, h in Hours)
+useC = @expression(model, sum(variableCostT[t]*EnergyProduction[h,t,r]/eff[t]
+    + fixedCostT[t]*(iniCapT[t,r] + AdditionalCapacity[t,r]) for t in TechNH, h in Hours, r in Region)
 )
-einvC = @expression(model, sum(AdditionalCapacity[t]*invCostT[t]*(1/expLifeTimeT[t]) for t in Tech))
-sinvC = @expression(model, sum(AdditionalStorage[s]*invCostS[s]*(1/expLifeTimeS[s]) for s in Storage))
-taxC = @expression(model, sum(EnergyProduction[h,t]*TAX*(1/proEmisFactor[t-1])/eff[t] for t in CarbonTech, h in Hours))
-rampExp = @expression(model, sum(((EnergyProduction[h,1] - EnergyProduction[h-1,1])^2)*1000 for h in Hours[Hours.>1]))
-
+einvC = @expression(model, sum(AdditionalCapacity[t,r]*invCostT[t]*(1/expLifeTimeT[t]) for t in TechNH, r in Region))
+sinvC = @expression(model, sum(AdditionalStorage[s,r]*invCostS[s]*(1/expLifeTimeS[s]) for s in Storage, r in Region))
+taxC = @expression(model, sum(EnergyProduction[h,t,r]*TAX*(1/proEmisFactor[t-1])/eff[t] for t in CarbonTech, h in Hours, r in Region))
+rampExp = @expression(model, sum(((EnergyProduction[h,1,r] - EnergyProduction[h-1,1,r])^2)*1000 for h in Hours[Hours.>1], r in Region))
+transInv = @expression(model, sum(AdditionalTrans[r, rr]*transInvCost*transLen[r,rr] for r in Region, rr in Region))
+transCost = @expression(model, sum(transCost*(Trans[h,r,rr]) for h in Hours, r in Region, rr in Region))
+hydroinvC = @expression(model, sum(AdditionalCapacity[7,r]*invCostT[7]*(1/(expLifeTimeT[7])) for r in Region))
 
 #OBJECTIVE
-@objective(model, Min , useC + einvC + sinvC + taxC + rampExp)
+@objective(model, Min , useC + einvC + sinvC + taxC + rampExp + transInv + transCost + hydroinvC)
 
 
 
@@ -96,21 +124,30 @@ termination_status(model)
 #RESULTS
 
 Results = Matrix{Float64}(undef, length(Hours), 15)
+NodesResults = zeros(length(Hours), length(Region), length(Region))
 for h in Hours
-    Results[h, 1] = value(EnergyProduction[h,1])
-    Results[h, 2] = value(EnergyProduction[h,2])
-    Results[h, 3] = value(EnergyProduction[h,3])
-    Results[h, 4] = value(EnergyProduction[h,4])
-    Results[h, 5] = value(EnergyProduction[h,5])
-    Results[h, 6] = value(EnergyProduction[h,6])
-    Results[h, 7] = value(EnergyProduction[h,7])
+    Results[h, 1] = value(EnergyProduction[h,1,1])
+    Results[h, 2] = value(EnergyProduction[h,2,1])
+    Results[h, 3] = value(EnergyProduction[h,3,1])
+    Results[h, 4] = value(EnergyProduction[h,4,1])
+    Results[h, 5] = value(EnergyProduction[h,5,1])
+    Results[h, 6] = value(EnergyProduction[h,6,1])
+    Results[h, 7] = value(EnergyProduction[h,7,1])
     #Results[h, 8] = value(EnergyProduction[h,8])
-    Results[h, 9] = value(Charge[1, h])
-    Results[h, 10] = value(Charge[2, h])
-    Results[h, 11] = value(Charge[3, h])
+    Results[h, 9] = value(Charge[1, h,1])
+    Results[h, 10] = value(Charge[2, h,1])
+    Results[h, 11] = value(Charge[3, h,1])
     Results[h, 12] = sum(Results[h, i] for i in 1:8)
     Results[h, 13] = sum(Results[h, i] for i in 9:11)
     #Results[h, 9] = Results[h,8]-Demand[h]
+end
+
+for h in Hours
+    for r in Region
+        for rr in Region
+            NodesResults[h,r,rr] = value.(Trans[h, r, rr])
+        end
+    end
 end
 
 plot(Results[:,1:7], label = ["Nuclear" "CHP" "CHP2" "Gas" "Wind" "PV" "Hydro"])
@@ -186,20 +223,20 @@ plot(additions)
 L = 100
 result = Matrix{Float64}(undef, L, 15)
 RENW = Matrix{Float64}(undef, length(Hours), 2)
+#k = collect(1::L)
 for i in 1:L
-    TAX = i*50
-    taxC = @expression(model, sum(EnergyProduction[h,t]*TAX*(1/proEmisFactor[t-1]) for t in CarbonTech, h in Hours))
-    @objective(model, Min , useC + einvC + sinvC + taxC + rampExp)
+    TAX = i*2
+    taxC = @expression(model, sum(EnergyProduction[h,t,r]*TAX*(1/proEmisFactor[t-1]) for t in CarbonTech, h in Hours, r in Region))
+    @objective(model, Min , useC + einvC + sinvC + taxC + rampExp + transInv + transCost)
     optimized = optimize!(model)
     result[i,1] = objective_value(model)
     for h in Hours
-        RENW[h,1] = value(EnergyProduction[h,5]) + value(EnergyProduction[h,6]) + value(EnergyProduction[h,7])
-        RENW[h,2] = value(EnergyProduction[h,2]) + value(EnergyProduction[h,3])+value(EnergyProduction[h,4])
+        RENW[h,1] = value.(sum(EnergyProduction[h,5,r] for r in Region)) + value.(sum(EnergyProduction[h,6,r] for r in Region)) + value.(sum(EnergyProduction[h,7,r] for r in Region))
+        RENW[h,2] = value.(sum(EnergyProduction[h,2,r] for r in Region)) + value.(sum(EnergyProduction[h,3,r] for r in Region)) + value.(sum(EnergyProduction[h,4,r] for r in Region))
     end
     result[i,2] = sum(RENW[h,1] for h in Hours)/sum(RENW[h,2]+RENW[h,1] for h in Hours )
 end
 
-plot(result[:,1])
 plot(result[:,2])
 
 
--- values.jl
+++ values.jl
@@ -9,34 +9,36 @@
 # 7 hydro
 
 #LENGTH OF INVESTIGATION
-LEN = 2000
+LEN = 7000
 
 ################################################################################
 # SETS
 
-Tech = collect(1:7)
-CarbonTech = collect(2:4)
-RenewableTech = collect(5:7)
-NonStorableTech = collect(1:6)
-Hours = collect(1:LEN)
-Storage = collect(1:3)
+Tech            = collect(1:7)
+CarbonTech      = collect(2:4)
+RenewableTech   = collect(5:7)
+TechNH          = collect(1:6)
+Hours           = collect(1:LEN)
+Storage         = collect(1:3)
+Region          = collect(1:5)
 
 
 
 ################################################################################
 # IMPORT DATA
 
-usage = CSV.read("data/usedata.csv") #RENEWABLES NINJA
-dema = CSV.read("data/demandinfinland.csv") #FINGRID (MWh)
-locfromend = 1000
+usage       = CSV.read("data/usedata.csv") #RENEWABLES NINJA
+dema        = CSV.read("data/demandinfinland.csv") #FINGRID (MWh)
+locfromend  = 1000
 
-sp = length(usage[:,1])-length(Hours)-locfromend
-ep  = length(usage[:,1])-locfromend
-udata = usage[sp:ep,2:7]
 
-sp = length(dema[:,1])-length(Hours)-locfromend-1
-ep  = length(dema[:,1])-locfromend-1
-dem = dema[sp:ep,5]
+sp      = length(usage[:,1])-length(Hours)-locfromend
+ep      = length(usage[:,1])-locfromend
+udata   = usage[sp:ep,2:7]
+
+sp      = length(dema[:,1])-length(Hours)-locfromend-1
+ep      = length(dema[:,1])-locfromend-1
+dem     = dema[sp:ep,5]
 
 #plot(udata[:,1])
 for h in Hours
@@ -51,16 +53,23 @@ end
 # PARAMETERS
 
 #TECH
-iniCapT = Array{Float64}(undef, length(Tech))
-maxCapT = Array{Float64}(undef, length(Tech))
-capFactor = Matrix{Float64}(undef, length(Hours), length(Tech))
-rampUpMax = Array{Float64}(undef, length(Tech))
-rampDownMax = Array{Float64}(undef, length(Tech))
-variableCostT = Array{Float64}(undef, length(Tech))
-fixedCostT = Array{Float64}(undef, length(Tech))
-invCostT = Array{Float64}(undef, length(Tech))
-expLifeTimeT = Array{Float64}(undef, length(Tech))
-eff = Array{Float64}(undef, length(Tech))
+iniCapT             = Matrix{Float64}(undef, length(Tech), length(Region))
+maxCapT             = Matrix{Float64}(undef, length(Tech), length(Region))
+capFactor           = zeros(length(Hours), length(Tech), length(Region))
+#capFactor = Matrix{Float64}(undef, length(Hours), length(Tech), length(Region))
+rampUpMax           = Array{Float64}(undef, length(Tech))
+rampDownMax         = Array{Float64}(undef, length(Tech))
+variableCostT       = Array{Float64}(undef, length(Tech))
+fixedCostT          = Array{Float64}(undef, length(Tech))
+invCostT            = Array{Float64}(undef, length(Tech))
+expLifeTimeT        = Array{Float64}(undef, length(Tech))
+eff                 = Array{Float64}(undef, length(Tech))
+hydroMinReservoir   = Array{Float64}(undef, length(Region))
+hydroMaxReservoir   = Array{Float64}(undef, length(Region))
+hydroReservoirCapacity = Array{Float64}(undef, length(Region))
+hydroInflow         = Matrix{Float64}(undef, length(Hours), length(Region))
+hydroMinEnvFlow     = Array{Float64}(undef, length(Region))
+hydroMaxOverall     = Array{Float64}(undef, length(Region))
 
 #for t in Tech
 #    iniCapT[t] = 400
@@ -75,42 +84,79 @@ eff = Array{Float64}(undef, length(Tech))
 #http://smartenergytransition.fi/en/
 #finnish-energy-system-can-be-made-100-fossil-fuel-free/
 
+
+#REGION RELATED
+for r in Region
+
+    # 1 nuclear
+    iniCapT[1, r] = 2794
+    maxCapT[1,r] = 10000 #random
+
+    # 2 coal slow
+    iniCapT[2,r] = 3200
+    maxCapT[2,r] = 10000 #not known
+
+    # 3 coal fast #not known
+    iniCapT[3,r] = 0
+    maxCapT[3,r] = 0
+
+    # 4 gas
+    iniCapT[4,r] = 1000 #"wiki"
+    maxCapT[4,r] = 50000
+
+    # 5 wind
+    iniCapT[5,r] = 2000
+    maxCapT[5,r] = 20000
+
+    # 6 solar
+    iniCapT[6,r] = 80 #wiki
+    maxCapT[6,r] = 15000 #not known
+
+    # 7 hydro
+    hydroMinReservoir[r]        = 2000 #TODO
+    hydroMaxReservoir[r]        = 8000 #TODO
+    hydroReservoirCapacity[r]   = 1000 #TODO
+    hydroMinEnvFlow[r]          = 500 #TODO
+    hydroMaxOverall[r]          = 3000
+    for h in Hours
+        hydroInflow[h,r]        = 700 .+rand(Float64,1)[1]*200 #TODO
+    end
+
+
+
+
+end
+
+#OTHERS
+
 # 1 nuclear
-iniCapT[1] = 2794
-maxCapT[1] = 10000 #random
-rampUpMax[1] = 1 #not known
-rampDownMax[1] = 1 #not known
-variableCostT[1] = 1 #or 37??
-fixedCostT[1] = 200 #not known
-invCostT[1] = 5000000
-expLifeTimeT[1] = 60 #not known
-eff[1] = 0.4
+rampUpMax[1]        = 1 #not known
+rampDownMax[1]      = 1 #not known
+variableCostT[1]    = 1 #or 37??
+fixedCostT[1]       = 200 #not known
+invCostT[1]         = 5000000
+expLifeTimeT[1]     = 60 #not known
+eff[1]              = 0.4
 
 # 2 coal slow
-iniCapT[2] = 3200
-maxCapT[2] = 10000 #not known
-rampUpMax[2] = 0.1 #not known
-rampDownMax[2] = 0.6 #not known
-variableCostT[2] = 11
-fixedCostT[2] = 100 #not known
-invCostT[2] = 1600000
-expLifeTimeT[2] = 40 #not known
-eff[2] = 0.45
+rampUpMax[2]        = 0.1 #not known
+rampDownMax[2]      = 0.6 #not known
+variableCostT[2]    = 11
+fixedCostT[2]       = 100 #not known
+invCostT[2]         = 1600000
+expLifeTimeT[2]     = 40 #not known
+eff[2]              = 0.45
 
 # 3 coal fast #not known
-iniCapT[3] = 0
-maxCapT[3] = 0
-rampUpMax[3] = 0.1
-rampDownMax[3] = 0.1
-variableCostT[3] = 11
-fixedCostT[3] = 100
-invCostT[3] = 1600000
-expLifeTimeT[3] = 50
-eff[3] = 0.4
+rampUpMax[3]        = 0.1
+rampDownMax[3]      = 0.1
+variableCostT[3]    = 11
+fixedCostT[3]       = 100
+invCostT[3]         = 1600000
+expLifeTimeT[3]     = 50
+eff[3]              = 0.4
 
 # 4 gas
-iniCapT[4] = 1000 #"wiki"
-maxCapT[4] = 50000
 rampUpMax[4] = 0.1 #not known
 rampDownMax[4] = 0.1 #not known
 variableCostT[4] = 22
@@ -120,8 +166,6 @@ expLifeTimeT[4] = 40 #not known
 eff[4] = 0.6
 
 # 5 wind
-iniCapT[5] = 2000
-maxCapT[5] = 20000
 rampUpMax[5] = 1 #not known
 rampDownMax[5] = 1 #not known
 variableCostT[5] = 0 #not known
@@ -131,8 +175,6 @@ expLifeTimeT[5] = 30 #not known
 eff[5] = 1
 
 # 6 solar
-iniCapT[6] = 80 #wiki
-maxCapT[6] = 15000 #not known
 rampUpMax[6] = 1 #not known
 rampDownMax[6] = 1 #not known
 variableCostT[6] = 0
@@ -142,8 +184,6 @@ expLifeTimeT[6] = 30 #not known
 eff[6] = 1 #not known
 
 # 7 hydro
-iniCapT[7] = 3100
-maxCapT[7] = 5000 #not known
 rampUpMax[7] = 1 #not known
 rampDownMax[7] = 1 #not known
 variableCostT[7] = 0
@@ -153,15 +193,18 @@ expLifeTimeT[7] = 50 #not known
 eff[7] = 0.7 #not known
 
 
-
+#Capacity factors for generation
 for h in Hours
-    capFactor[h, 1] = 1
-    capFactor[h, 2] = 1
-    capFactor[h, 3] = 1
-    capFactor[h, 4] = 1
-    capFactor[h, 5] = udata[h,5]
-    capFactor[h, 6] = udata[h,6]
-    capFactor[h, 7] = ((sin((h+3)/5))^2)./2 .+0.3 .+rand(Float64,1)[1]*0.2 #TO BE CONSTRUCTED BETTER
+    for r in Region #TODO
+        capFactor[h, 1, r] = 1
+        capFactor[h, 2, r] = 1
+        capFactor[h, 3, r] = 1
+        capFactor[h, 4, r] = 1
+        capFactor[h, 5, r] = udata[h,5]
+        capFactor[h, 6, r] = udata[h,6]
+        capFactor[h, 7, r] = ((sin((h+3)/5))^2)./2 .+0.3 .+rand(Float64,1)[1]*0.2 #TO BE CONSTRUCTED BETTER
+
+    end
 end
 
 #OTHER
@@ -174,28 +217,55 @@ end
 
 
 #STORAGE
-chargeMax = Array{Float64}(undef, length(Storage))
-dischargeMax = Array{Float64}(undef, length(Storage))
-iniCapS = Array{Float64}(undef, length(Storage))
-maxCapS = Array{Float64}(undef, length(Storage))
-invCostS = Array{Float64}(undef, length(Storage))
-expLifeTimeS = Array{Float64}(undef, length(Storage))
+chargeMax       = Array{Float64}(undef, length(Storage))
+dischargeMax    = Array{Float64}(undef, length(Storage))
+iniCapS         = Matrix{Float64}(undef, length(Storage), length(Region))
+maxCapS         = Matrix{Float64}(undef, length(Storage), length(Region))
+invCostS        = Array{Float64}(undef, length(Storage))
+expLifeTimeS    = Array{Float64}(undef, length(Storage))
 
 for s in Storage
+    for r in Region #TODO
+        iniCapS[s,r] = 800
+        maxCapS[s,r] = 1000
+    end
     chargeMax[s] = 500
     dischargeMax[s] = 800
-    iniCapS[s] = 800
-    maxCapS[s] = 1000
-    invCostS[s] = 40000
+    invCostS[s]     = 40000
     expLifeTimeS[s] = 20
 end
 
 # DEMAND
-Demand = Array{Float64}(undef, length(Hours))
+Demand = Matrix{Float64}(undef, length(Hours), length(Region))
 
 for h in Hours
-    Demand[h] = udata[h,4]
+    for r in Region
+        Demand[h, r] = udata[h,4] #TODO
+    end
+
 end
+
+transCap = Matrix{Float64}(undef, length(Region), length(Region))
+maxTransCap = Matrix{Float64}(undef, length(Region), length(Region))
+transLen = Matrix{Float64}(undef, length(Region), length(Region))
+for r in Region
+    for rr in Region
+        if rr == r
+            transCap[r, rr]     = 0
+            maxTransCap[r,rr]   = 0
+            transLen[r, rr]     = 0
+         else
+            transCap[r, rr]     = 500
+            maxTransCap[r,rr]   = 700
+            transLen[r, rr]     = 500
+        end
+    end
+end
+
+transInvCost = 30
+transCost    = 5
+
+
 
 #plot(capFactor)
 #plot(Demand)
